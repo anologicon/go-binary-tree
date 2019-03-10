adab05b8eeeec1c8a47689ef7b4d64250698f364
Anologicon <pauloraimundi2009@gmail.com>
2019-03-09 22:32:02 -0300

    Metodo print

--- main.go
+++ main.go
@@ -8,24 +8,13 @@ import (
 	"interfaces"
 )
 
-// BinaryNode is a model for node
-type BinaryNode = model.BinaryNode
-
-// BinaryTree is a model for Tree
-type BinaryTree = model.BinaryTree
-
-// Profundidade is a model to aply the 'deep search'
-type Profundidade = search.Profundidade 
-
-type iPrint = interfaces.IPrint
-
-func print(t *BinaryTree, objectPrinter iPrint) {
-
+// Print - metodo que executa a exibição dos dados
+func Print(t *model.BinaryTree, objectPrinter interfaces.IPrint) {
 	objectPrinter.Print(os.Stdout, t.Root, 0, 'M')
 }
 
 func main() {
-	tree := new(BinaryTree)
+	tree := new(model.BinaryTree)
 	tree.Insert(5).
 		Insert(3).
 		Insert(4).
@@ -34,5 +23,5 @@ func main() {
 		Insert(17).
 		Insert(12)
 
-	print(tree, new(Profundidade))
+	Print(tree, new(search.Profundidade))
 }
